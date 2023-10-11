151a2bfe45e79c63ebe6a6a08cde475789da7989
Maciej Laskowski <maciek30088@gmail.com>
2023-10-11 16:52:47 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,175 @@
+const inquirer = require('inquirer');
+const consola = require('consola');
+enum Action {
+  List = 'list',
+  Add = 'add',
+  Remove = 'remove',
+  Quit = 'quit',
+}
+
+type InquirerAnswers = {
+  action: Action;
+};
+
+enum MessageVariant {
+  Success = 'success',
+  Error = 'error',
+  Info = 'info',
+}
+class Message {
+  constructor(private content: string) {}
+
+
+  public show() {
+    console.log(this.content);
+  }
+
+  public capitalize() {
+    this.content =
+      this.content.charAt(0).toUpperCase() +
+      this.content.slice(1).toLowerCase();
+  }
+
+  public toUpperCase() {
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase() {
+    this.content = this.content.toLowerCase();
+  }
+
+  public static showColorized(variant: MessageVariant, text: string) {
+    switch (variant) {
+      case MessageVariant.Success:
+        consola.success(text);
+        break;
+      case MessageVariant.Error:
+        consola.error(text);
+        break;
+      case MessageVariant.Info:
+        consola.info(text);
+        break;
+      default:
+        console.error('Invalid variant.');
+    }
+  }
+}
+type User = {
+  name: string;
+  age: number;
+};
+
+class UsersData {
+  private data: User[] = [];
+
+  
+  public showAll(): void {
+    Message.showColorized(MessageVariant.Info, 'Users data');
+    if (this.data.length > 0) {
+      console.table(this.data);
+    } else {
+      Message.showColorized(MessageVariant.Error, 'No data...');
+    }
+  }
+
+  public add(user: User) {
+    if (this.isValidUser(user)) {
+      this.data.push(user);
+      Message.showColorized(
+        MessageVariant.Success,
+        'User has been successfully added!'
+      );
+    } else {
+      Message.showColorized(MessageVariant.Error, 'Wrong data!');
+    }
+  }
+
+  public remove(userName: string) {
+    const index = this.data.findIndex((user) => user.name === userName);
+    if (index !== -1) {
+      this.data.splice(index, 1);
+      Message.showColorized(MessageVariant.Success, 'User deleted!');
+    } else {
+      Message.showColorized(MessageVariant.Error, 'User not found...');
+    }
+  }
+
+  private isValidUser(user: User): boolean {
+    return user.age > 0 && user.name.length > 0;
+  }
+}
+
+// const users = new UsersData();
+// users.showAll();
+// users.add({ name: 'Jan', age: 20 });
+// users.add({ name: 'Adam', age: 30 });
+// users.add({ name: 'Kasia', age: 23 });
+// users.add({ name: 'Basia', age: -6 });
+// users.showAll();
+// users.remove('Maurycy');
+// users.remove('Adam');
+// users.showAll();
+
+const startApp = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'input',
+        message: 'How can I help you?',
+      },
+    ])
+    .then(async (answers: InquirerAnswers) => {
+      switch (answers.action) {
+        case Action.List:
+          users.showAll();
+          break;
+        case Action.Add:
+          const user = await inquirer.prompt([
+            {
+              name: 'name',
+              type: 'input',
+              message: 'Enter name',
+            },
+            {
+              name: 'age',
+              type: 'number',
+              message: 'Enter age',
+            },
+          ]);
+          users.add(user);
+          break;
+        case Action.Remove:
+          const name = await inquirer.prompt([
+            {
+              name: 'name',
+              type: 'input',
+              message: 'Enter name',
+            },
+          ]);
+          users.remove(name.name);
+          break;
+        case Action.Quit:
+          Message.showColorized(MessageVariant.Info, 'Bye bye!');
+          return;
+      }
+
+      startApp();
+    });
+};
+
+const users = new UsersData();
+
+console.log('\n');
+console.info('???? Welcome to the UsersApp!');
+console.log('====================================');
+Message.showColorized(MessageVariant.Info, 'Available actions');
+console.log('\n');
+console.log('list – show all users');
+console.log('add – add new user to the list');
+console.log('remove – remove user from the list');
+console.log('quit – quit the app');
+console.log('\n');
+
+
+startApp();
